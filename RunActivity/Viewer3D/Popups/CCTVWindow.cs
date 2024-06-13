fff50c35e1e92e35cda2d6b9523deeffa25f35f6
Er Pe <spammer@montefaro.eu>
2024-06-13 14:05:49 +0200

    Carta de ajuste en CCTV

--- RunActivity/Viewer3D/Popups/CCTVWindow.cs
+++ RunActivity/Viewer3D/Popups/CCTVWindow.cs
@@ -2,7 +2,10 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +15,19 @@ namespace Tourmaline.Viewer3D.Popups
     {        
 
         Label Title;
+        CameraControl mvarControl;
 
         public CCTVWindow(WindowManager owner)
             : base(owner, 640, Window.DecorationSize.Y + 480, "CCTV")
         {
+            mvarControl = new CameraControl(630, 480);
 
+
+        }
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            mvarControl.Draw(spriteBatch, new Point(0, 0));
         }
 
 
@@ -29,16 +40,25 @@ namespace Tourmaline.Viewer3D.Popups
             : base(0,0,width, height)
             {
 
-            }
 
-        internal override void Draw(SpriteBatch spriteBatch, Point offset)
+        }
+
+        internal override void Draw(SpriteBatch canvas, Point offset)
         {
             //Primer acercamiento... vamos a intentar cargar una imagen fija
-            if(null ==CanvasTexture)
+            if (null == CanvasTexture)
             {
-
+                //Cargamos la textura por primera vez.
+                string resourcesFile = Tourmaline.Viewer3D.Processes.Game.Instance.ResourcesPath;
+                string cartaFile = Path.Combine(resourcesFile, "SetView.png");
+                if (File.Exists(cartaFile))
+                {
+                    CanvasTexture = new Texture2D(canvas.GraphicsDevice, 640, 480, false, SurfaceFormat.Color);
+                    CanvasTexture = SharedTextureManager.Get(canvas.GraphicsDevice, cartaFile);
+                }
             }
-
+            Debug.Assert(null != CanvasTexture);
+            canvas.Draw(CanvasTexture,new Rectangle(4,56,632,472),Color.White);
         }
 
 
